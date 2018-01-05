f06549717d4ae602d08e9bc249e1994462e6dd7c
zhengya <zhengya123>
2018-01-05 15:13:33 +0800

    Initial Commit

---
+++ PTZVC_Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PTZVC_Test
+//
+//  Created by 中商国际 on 2018/1/5.
+//  Copyright © 2018年 中商国际. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PTZVC_Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PTZVC_Test
+//
+//  Created by 中商国际 on 2018/1/5.
+//  Copyright © 2018年 中商国际. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
